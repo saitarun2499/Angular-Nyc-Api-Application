9462a5c85c0d87484a38d581276b25f68c46ce06
Singaraju Sriram Teja <ssriramteja@gmail.com>
2020-08-23 12:20:24 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import {DataService} from "./data.service";
 
 @Component({
-  selector: 'app-books',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
 export class AppComponent implements OnInit {
-  books:[];
+  books;
   displayedColumns: string[] = ['TopSellers','AuthorName','Title'];
   constructor(private http1:DataService) { }
   ngOnInit(){
     this.http1.authors().subscribe(
     (data)=>{
     console.log(data),
-    this.books=data['results']['books']})
+    this.books=data['results']})
     console.log(this.books);
     }
-  
 }
